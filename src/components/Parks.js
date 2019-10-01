9ef6991048ae5b3e66ccf7082f6034f072d2a428
Levi Villarreal <villarreallevi@gmail.com>
2019-10-01 15:59:01 -0500

    add pages for each of the 3 models

---
+++ src/components/Parks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Parks extends React.Component {
+  render() {
+    return (
+      <h2>National Parks</h2>
+    );
+  }
+}
+
+export default Parks;
---
+++ src/components/States.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class States extends React.Component {
+  render() {
+    return (
+      <h2>Locations</h2>
+    );
+  }
+}
+
+export default States;
