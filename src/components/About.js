f19a5360a8fcc62ba5eaaaea9a50ab28e4601352
Miles Chandler <miles.chandler@contactrelief.com>
2019-10-03 14:48:33 -0500

    Added team images and fixed name bug

--- src/components/About.js
+++ src/components/About.js
@@ -4,59 +4,88 @@ import TeamMember from './TeamMember'
 class About extends React.Component {
   state = {
     team: [],
-    issues: []
+    issues: [],
+    load: false
   }
 
+  removeCustomers(data) {
+    var teamMembers = []
+    let i;
+    for(i = 0; i < data.length; ++i) {
+      var member = data[i]
+      if (member.access_level === 40) {
+        teamMembers.push(member)
+      }
+    }
+    this.setState({
+      team:teamMembers
+    })
+  }
+
+  transformName(name){
+    return name.toLowerCase().replace(/\s/g, '');
+  }
   aggregateDuplicates(data) {
     // if we have duplicate names just combine their total commits
     var dict = {}
-    var aggedData = []
-    //console.log('data', data[0].name)
     let i;
     for (i = 0; i < data.length; i++) {
       var member = data[i]
-      if (dict[member.name]){
-        dict[member.name] += member.commits
+      var name = this.transformName(member.committer_name)
+      if (dict[name]){
+        dict[name] += 1
       } else {
-        dict[member.name] = member.commits
+        dict[name] = 1
       }
     }
-    aggedData = []
-    for (var key in dict) {
-      var teamMember = {
-        name: key,
-        commits: dict[key]
+    var teamWithCommits = []
+    let j;
+    for (j = 0; j < this.state.team.length; ++j) {
+      member = this.state.team[j]
+      name = this.transformName(member.name)
+      var numCommits = dict[name]
+      var memberWithCommits = {
+        ...member,
+        commits: numCommits
       }
-      aggedData.push(teamMember)
+      teamWithCommits.push(memberWithCommits)
     }
     this.setState({
-      team: aggedData
+      team:teamWithCommits
     })
   }
   async componentDidMount() {
-    
-    await fetch('https://gitlab.com/api/v4/projects/14563233/issues')
+    await fetch('https://gitlab.com/api/v4/projects/14563233/members', 
+    {headers:{'PRIVATE-TOKEN': 'wbRqCM6kGxhr5x98_dHj'}})
     .then(res => res.json())
     .then((data) => {
-      this.setState({issues:data})
+      this.removeCustomers(data)
     })
     
-    await fetch('https://gitlab.com/api/v4/projects/14563233/repository/contributors')
+    await fetch('https://gitlab.com/api/v4/projects/14563233/repository/commits')
     .then(res => res.json())
     .then((data) => {
       this.aggregateDuplicates(data)
     })
-    
+    await fetch('https://gitlab.com/api/v4/projects/14563233/issues')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        issues:data,
+        load: true
+      })
+    })
+    console.log(this.state)
   }
+
   render() {
     var issueStats = this.state.issues
     return (
       // Use React.Fragment because there are multiple HTML top-level elements
       <React.Fragment>
         <h2>About</h2>
-
-        <ul>
-          {this.state.team.map(function(member, index){
+        {this.state.load &&(<ul>
+          {this.state.team.map(function(member){
             // Return a single team member component
             return (
               <li>
@@ -64,7 +93,8 @@ class About extends React.Component {
               </li>
             );
           })}
-        </ul>
+        </ul>)}
+        
       </React.Fragment>
     );
   }
--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -7,14 +7,22 @@ class TeamMember extends React.Component {
             name: this.props.member.name,
             commits: this.props.member.commits,
             issues: this.props.issues,
-            issuesClosed: 0
+            issuesClosed: 0,
+            imgURL: this.props.member.avatar_url
         }
     }
 
     componentDidMount() {
         this.countIssues()
+        this.setState({
+            name:this.props.member.name,
+            commits: this.props.member.commits,
+            issues: this.props.issues,
+            imgURL:this.props.member.avatar_url
+        })
     }
-    
+
+
     countIssues(){
         var closedIssues = 0
         this.state.issues.forEach(issue => {
@@ -29,9 +37,11 @@ class TeamMember extends React.Component {
         })
     }
     render() {
+        var alt = 'https://cdn.imgbin.com/5/19/23/imgbin-monterey-bay-aquarium-gary-park-ranger-forest-ranger-s-vhPH4MdLUQsTuFtSvF9QiFkmy.jpg'
         return (
         <React.Fragment>
             <h3>{this.state.name}</h3>
+            <img src={this.state.imgURL} alt={alt}></img>
             <p>Commits: {this.state.commits}</p>
             <p>Issues: {this.state.issuesClosed}</p>
         </React.Fragment>
