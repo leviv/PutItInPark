dd055d6fabe2bcffc19c763fa5948915cb9a579d
Levi Villarreal <villarreallevi@gmail.com>
2019-10-20 16:02:35 -0500

    Style navbar and add site logo/favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Logo from '../assets/img/logo.png';
 
 class Header extends React.Component {
 
@@ -7,7 +8,7 @@ class Header extends React.Component {
     return (
       <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="/">Put It In Park</a>
+          <a className="navbar-brand" href="/"><img src={Logo} alt="Icon with mountains and the sun" width="40px"/></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -15,16 +16,16 @@ class Header extends React.Component {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/about/">About</Link>
+                <Link className="nav-link" to="/about/"><h5>About</h5></Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/parks/">National Parks</Link>
+                <Link className="nav-link" to="/parks/"><h5>National Parks</h5></Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/recreation/">Recreation</Link>
+                <Link className="nav-link" to="/recreation/"><h5>Recreation</h5></Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/states/">States</Link>
+                <Link className="nav-link" to="/states/"><h5>States</h5></Link>
               </li>
             </ul>
           </div>
--- src/components/ParkCard.js
+++ src/components/ParkCard.js
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 class ParkCard extends React.Component {
   render() {
     return (
-      <div className="instance-card"
-           style={{ backgroundImage: `url(${this.props.imageUrl})`}}
-           >
-        <div class="hidden-details">
-          <p>State: {this.props.state}</p>
-          <p>Status: {this.props.status}</p>
+      <Link to="/states/">
+        <div className="instance-card"
+             style={{ backgroundImage: `url(${this.props.imageUrl})`}}
+             >
+          <div class="hidden-details">
+            <p>State: {this.props.state}</p>
+            <p>Status: {this.props.status}</p>
+          </div>
+          <div className="details">
+            <p>{this.props.name}</p>
+          </div>
         </div>
-        <div className="details">
-          <p>{this.props.name}</p>
-        </div>
-      </div>
+      </Link>
     );
   }
 }
