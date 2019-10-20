f65a570a06babd6233394ee003934add5386071f
Levi Villarreal <villarreallevi@gmail.com>
2019-10-20 18:51:57 -0500

    Fix splash page model photos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Splash from '../assets/img/splash.png';
+import Yosemite from '../assets/img/yosemite.jpg';
+import Climbing from '../assets/img/climbing.jpg';
+import Texas from '../assets/img/texas.jpg';
 
 class Home extends React.Component {
   render() {
@@ -27,19 +30,19 @@ class Home extends React.Component {
             <h1 className="text-center">Explore Parks, States, and Activities!</h1>
             <div className="row">
               <div className="col-md-4 model-splash">
-                <img src="https://www.nps.gov/common/uploads/grid_builder/yose/crop16_9/2A84C724-1DD8-B71B-0B0F4361A736E640.jpg?width=950&quality=90&mode=crop" alt="National park landscape"/>
+                <img src={Yosemite} alt="National park landscape"/>
                 <h3>National Parks</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt veniam ipsum aliquam, illo dignissimos. Quaerat adipisci culpa, odit, provident non inventore nesciunt deleniti at earum qui placeat dolorem labore similique.</p>
                 <Link to="/parks/"><button className="button button-secondary">Explore Parks</button></Link>
               </div>
               <div className="col-md-4 model-splash">
-                <img src="https://www.nps.gov/npgallery/GetAsset/F77E8BB4-155D-451F-670C6F80B88A153E/proxy/hires?" alt="A man climbing outdoors"/>
+                <img src={Climbing} alt="A man climbing outdoors"/>
                 <h3>Recreational Activities</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ab iste, non quo. Et rerum cum placeat reiciendis corrupti necessitatibus perferendis eaque dolore cumque, maxime non, dignissimos libero debitis repellat?</p>
                 <Link to="/recreation/"><button className="button button-secondary">Explore Activities</button></Link>
               </div>
               <div className="col-md-4 model-splash">
-                <img src="https://www.nps.gov/npgallery/GetAsset/0005A3F1-1DD8-B71B-0B38A6233082EC97/proxy/hires?" alt="A state flag"/>
+                <img src={Texas} alt="Texas state flag"/>
                 <h3>States</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum odio, quo aliquid asperiores optio quia debitis facere nemo officiis possimus, enim voluptates numquam autem accusamus quibusdam iste, placeat vitae delectus.</p>
                 <Link to="/states/"><button className="button button-secondary">Explore States</button></Link>
