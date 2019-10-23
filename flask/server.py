078be1b2d797bcd3d54534a633c384aef5e89245
jengelhart <alexanderengelhart713@gmail.com>
2019-10-23 17:09:16 -0500

    flask WIP

---
+++ flask/server.py
@@ -0,0 +1,24 @@
+import flask
+import flask_sqlalchemy
+import flask_restless
+
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root@/models?unix_socket=/cloudsql/potent-retina-254722:putitinpark' #need to test
+db = flask_sqlalchemy.SQLAlchemy(app)
+
+class Location(db.Model):
+    __table__ = db.Model.metadata.tables['location']
+
+class Nationalparks(db.Model):
+    __table__ = db.Model.metadata.tables['nationalparks']
+
+class Recreation(db.Model):
+    __table__ = db.Model.metadata.tables['recreation']
+
+manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
+manager.create_api(Location, methods=['GET'])
+manager.create_api(Nationalparks, methods=['GET'])
+manager.create_api(Recreation, methods=['GET'])
+
+app.run()
