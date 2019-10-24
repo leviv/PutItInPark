7e2708e3cc4b6694f3ae5b504fa147743766613e
Miles Chandler <miles.chandler@contactrelief.com>
2019-10-24 13:42:13 -0500

    Backend api working

--- flask/server.py
+++ flask/server.py
@@ -2,6 +2,8 @@ import flask
 import flask_sqlalchemy
 import flask_restless
 
+
+
 '''import pymysql                                                                   
 connection = pymysql.connect(host='127.0.0.1',                                   
                              user='root',                                        
@@ -12,18 +14,25 @@ old str = 'mysql+pymysql://root:SWE10PutItInPark@/models?unix_socket=/cloudsql/p
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:SWE10PutItInPark@127.0.0.1/models'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:SWE10PutItInPark@127.0.0.1/models'#'mysql+pymysql://root:SWE10PutItInPark@127.0.0.1/models'
 db = flask_sqlalchemy.SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
 
-class Location(db):
-    __table__ = db.Model.metadata.tables['location']
+class Location(db.Model):
+    __tablename__ = 'location'
+    __table_args__ = {'extend_existing': True} 
+    fips = db.Column(db.Integer, primary_key=True)
 
 class Nationalparks(db.Model):
-    __table__ = db.Model.metadata.tables['nationalparks']
+    __tablename__ = 'recreation'
+    __table_args__ = {'extend_existing': True} 
+    rec_id = db.Column(db.Integer, primary_key=True)
 
 class Recreation(db.Model):
-    __table__ = db.Model.metadata.tables['recreation']
+    __tablename__ = 'nationalparks'
+    __table_args__ = {'extend_existing': True} 
+    park_code = db.Column(db.Unicode, primary_key=True)
+
 
 db.create_all()
 
