bd2ac39846211ec0ca6ed538dd650227defccc7a
billyvo <billyvo@utexas.edu>
2019-10-24 10:40:16 -0500

    server.py set to connect to proxy (must run cloudsql_proxy)

--- flask/server.py
+++ flask/server.py
@@ -2,9 +2,17 @@ import flask
 import flask_sqlalchemy
 import flask_restless
 
+'''import pymysql                                                                   
+connection = pymysql.connect(host='127.0.0.1',                                   
+                             user='root',                                        
+                             password='SWE10PutItInPark',                        
+                             db='models')
+                             
+old str = 'mysql+pymysql://root:SWE10PutItInPark@/models?unix_socket=/cloudsql/potent-retina-254722:us-central1:putitinpark' '''
+
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:SWE10PutItInPark@/models?unix_socket=/cloudsql/potent-retina-254722:us-central1:putitinpark'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:SWE10PutItInPark@127.0.0.1/models'
 db = flask_sqlalchemy.SQLAlchemy(app)
 db.Model.metadata.reflect(db.engine)
 
