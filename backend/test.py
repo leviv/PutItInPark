3a1010ac6950f104fd7f0059af39a87450c56a0d
jengelhart <alexanderengelhart713@gmail.com>
2019-10-24 20:05:31 -0500

    python unit tests

--- backend/test.py
+++ backend/test.py
@@ -10,34 +10,34 @@ park_json = requests.get(url + "/nationalparks").json()
 
 class BackendTests (TestCase) :
     def test1 (self) :
-        self.assertEqual(resp_json['num_results'], 51)
+        self.assertEqual(loc_json['num_results'], 51)
 
     def test2 (self) :
-        self.assertEqual(6, 6)
+        self.assertEqual(loc_json['objects'][0]['name'], 'Alabama')
 
     def test3 (self) :
-        self.assertEqual(7, 7)
+        self.assertEqual(loc_json['objects'][1]['name'], 'Alabama')
 
     def test4 (self) :
-        self.assertEqual(1, 1)
+        self.assertEqual(rec_json['num_results'], 210)
 
     def test5 (self) :
-        self.assertEqual(6, 6)
+        self.assertEqual(rec_json['objects'][0]['location'], 'New-Mexico')
 
     def test6 (self) :
-        self.assertEqual(7, 7)
+        self.assertEqual(rec_json['objects'][1]['location'], 'Alaska')
     
     def test7 (self) :
-        self.assertEqual(1, 1)
+        self.assertEqual(park_json['num_results'], 49)
 
     def test8 (self) :
-        self.assertEqual(6, 6)
+        self.assertEqual(park_json['objects'][0]['location'], 'Maine')
 
     def test9 (self) :
-        self.assertEqual(7, 7)
+        self.assertEqual(park_json['objects'][1]['location'], 'Utah')
 
     def test10 (self) :
-        self.assertEqual(1, 1)
+        self.assertEqual(park_json['objects'][2]['location'], 'South-Dakota')
 
 if __name__ == "__main__" :
     main()
