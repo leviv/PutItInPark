2c03be2d06a6e8172a67f6627a9a924544d749d6
ellarobertson <ella.a.robertson@gmail.com>
2019-11-03 23:58:56 -0600

    Wrote script to get rec areas for states that don't have national parks

---
+++ misc/DB/scripts/location/recfromstate.py
@@ -0,0 +1,44 @@
+import urllib.request, json
+
+#retrieve basic recreational area information
+
+class recreation_area:
+    #later add activities, num activities, imglink
+    def __init__(self, rec_id, rec_name, location, natpark, reservable, lat, lon, description, parent_org_id, staylimit):
+        self.rec_id = rec_id
+        self.rec_name = rec_name
+        self.location = location
+        self.natpark = natpark
+        self.reservable = reservable
+        self.lat = lat
+        self.lon = lon
+        self.description = description
+        self.parent_org = parent_org_id
+        self.staylimit = staylimit
+    
+    def __str__(self):
+        pt1 = str(self.rec_id) + '|' + str(self.rec_name) + '|' + str(self.location) + '|'
+        pt2 = str(self.natpark) + '|' + str(reservable) + '|' + str(self.lat) + '|' + str(self.lon) + '|'
+        pt3 = str(self.description) + '|' + str(self.parent_org) + '|' + str(self.staylimit).strip()
+        return pt1 + pt2 + pt3
+
+states = open("locationsnonatpark.txt", "r")
+
+#rec id is key, rec_areas class is val
+recs = {}
+
+apikeyrec = '233a1540-c404-419b-b60c-ab4a096cc7d2'
+
+for line in states:
+    state = line.strip()
+    with urllib.request.urlopen('https://ridb.recreation.gov/api/v1/recareas?limit=50&offset=0&state=' + state + '&apikey=' + apikeyrec) as url:
+        rec = json.loads(url.read().decode())
+        meta = rec.get('METADATA').get('RESULTS').get('CURRENT_COUNT')
+        rec_areas = rec.get("RECDATA")
+        allareas = ""
+        for area in rec_areas:
+            rec_id = area.get('RecAreaID')
+            allareas += rec_id + ","
+        allareas.strip(",")
+        print(state + " " + str(meta), end = " ")
+        print(allareas)
---
+++ misc/DB/scripts/recreation/recareasbystate.py
@@ -0,0 +1,53 @@
+import urllib.request, json
+
+#retrieve basic recreational area information
+
+class recreation_area:
+    #later add activities, num activities, imglink
+    def __init__(self, rec_id, rec_name, location, natpark, reservable, lat, lon, description, parent_org_id, staylimit):
+        self.rec_id = rec_id
+        self.rec_name = rec_name
+        self.location = location
+        self.natpark = natpark
+        self.reservable = reservable
+        self.lat = lat
+        self.lon = lon
+        self.description = description
+        self.parent_org = parent_org_id
+        self.staylimit = staylimit
+    
+    def __str__(self):
+        pt1 = str(self.rec_id) + '|' + str(self.rec_name) + '|' + str(self.location) + '|'
+        pt2 = str(self.natpark) + '|' + str(reservable) + '|' + str(self.lat) + '|' + str(self.lon) + '|'
+        pt3 = str(self.description) + '|' + str(self.parent_org) + '|' + str(self.staylimit).strip()
+        return pt1 + pt2 + pt3
+
+states = open("locationsnonatpark.txt", "r")
+
+#rec id is key, rec_areas class is val
+recs = {}
+
+apikeyrec = '233a1540-c404-419b-b60c-ab4a096cc7d2'
+
+for line in states:
+    state = line.strip()
+    with urllib.request.urlopen('https://ridb.recreation.gov/api/v1/recareas?limit=50&offset=0&state=' + state + '?&apikey=' + apikeyrec) as url:
+        rec_area = json.loads(url.read().decode())
+        rec_id = rec_area.get('RecAreaID')
+        loc = state
+        natpark = ""
+        parent_id = rec_area.get('ParentOrgID')
+        pre_name = str(rec_area.get('RecAreaName'))
+        name = pre_name.replace(' ', '-')
+        pre_description = str(rec_area.get('RecAreaDescription'))
+        description = pre_description.replace('\n', "  ").replace('<p>', " ").replace('</p>', " ").replace("<em>", " ").replace('</em>', " ")
+        lon = rec_area.get('RecAreaLongitude')
+        lat = rec_area.get('RecAreaLatitude')
+        staylimit = True
+        if(rec_area.get("StayLimit") == ""):
+            staylimit = False
+        reservable = rec_area.get('Reservable')
+        recs[rec_id] = recreation_area(rec_id, name, loc, natpark, reservable, lat, lon, description, parent_id, staylimit)
+
+for value in recs.values():
+    print(str(value))
