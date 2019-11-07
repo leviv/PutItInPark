c830c906622b17b3cfdcb1cdea3e91b13dfb0d25
Levi Villarreal <leviv@wireless-10-147-56-197.public.utexas.edu>
2019-11-06 18:44:03 -0600

    Fix python tests for now

--- backend/test.py
+++ backend/test.py
@@ -2,14 +2,14 @@ from unittest import main, TestCase
 import json
 import requests
 
-url = "https://flask-backend-dot-potent-retina-254722.appspot.com"
+url = "https://flask-backend-dot-potent-retina-254722.appspot.com/api"
 loc_json = requests.get(url + "/locations").json()
 rec_json = requests.get(url + "/recreations").json()
 park_json = requests.get(url + "/nationalparks").json()
 
 class BackendTests(TestCase):
     def test1(self):
-        self.assertEqual(len(loc_json), 50)
+        self.assertEqual(len(loc_json['objects']), 10)
 
 if __name__ == "__main__":
     main()
