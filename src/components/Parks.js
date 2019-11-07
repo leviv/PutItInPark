f54557f5425830786ece9b413cfe59d7f439b864
Levi Villarreal <leviv@wireless-10-147-56-197.public.utexas.edu>
2019-11-06 18:37:16 -0600

    Add elements and styles for searching sorting and filtering

--- src/components/Parks.js
+++ src/components/Parks.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ParkCard from './ParkCard';
 import ReactPaginate from 'react-paginate';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
-const API_ENDPOINT = "https://flask-backend-dot-potent-retina-254722.appspot.com/nationalparks";
+const API_ENDPOINT = "https://flask-backend-dot-potent-retina-254722.appspot.com/api/nationalparks";
 
 class Parks extends React.Component {
   handlePageClick = data => {
@@ -33,13 +35,13 @@ class Parks extends React.Component {
   }
 
   makeApiCall(pageNumber) {
-    fetch(API_ENDPOINT + "/page=" + pageNumber)
+    fetch(API_ENDPOINT + "?page=" + pageNumber)
 
       // Transform the data into json
       .then((resp) => resp.json())
       .then((data) => {
         // Process data
-        data.forEach((park) => {
+        data.objects.forEach((park) => {
           park.rec_ids = park.rec_ids.split(",");
           this.state.parks.push(park);
         });
@@ -67,6 +69,52 @@ class Parks extends React.Component {
         </div>
 
         <div className="container">
+          <div className="collapsed" id="augment-container">
+            <div className="row search-row">
+              <div className="model-search">
+                <h4 className="model-search-component">Find</h4>
+                <input className="form-control model-search-component" type="search" placeholder="Park" aria-label="Park Search"/>
+                <FontAwesomeIcon icon={faSearch} className="model-search-component"/>
+              </div>
+              <FontAwesomeIcon icon={faAngleDown} id="carat" onClick={expandFilters}/>
+            </div>
+            <div className="row">
+              <div className="col-md-8 model-filter">
+                <h4>Filter by</h4>
+                <select class="form-control" id="filter1">
+                  <option selected disabled>Num of Recreation Areas</option>
+                  <option>0-10</option>
+                  <option>10-20</option>
+                  <option>20+</option>
+                </select>
+                <select class="form-control" id="filter2">
+                  <option selected disabled>Park Fee</option>
+                  <option>Free</option>
+                  <option>Not Free</option>
+                </select>
+                <select class="form-control" id="filter3">
+                  <option selected disabled>Number of Visitors</option>
+                  <option>0-50,000</option>
+                  <option>50,000-100,000</option>
+                  <option>100,000+</option>
+                </select>
+              </div>
+              <div className="col-md-4 model-filter">
+                <h4>Sort by</h4>
+                <select class="form-control" id="sort1">
+                  <option selected disabled>Pick One</option>
+                  <option>Park Name</option>
+                  <option>Location</option>
+                  <option>Num of Recreation Areas</option>
+                  <option>Fee</option>
+                  <option>Number of Visitors</option>
+                </select>
+              </div>
+            </div>
+            <div className="text-center">
+              <button className="button button-secondary">Apply Filters</button>
+            </div>
+          </div>
 
           {row.map((result, index) => {
             return (
@@ -109,4 +157,21 @@ class Parks extends React.Component {
     );
   }
 }
+
+function expandFilters () {
+  const filters= document.getElementById("augment-container");
+  if (filters.classList.contains("collapsed")) {
+    const sectionHeight = filters.scrollHeight;
+
+    filters.style.height = sectionHeight + "px";
+
+    filters.classList.remove("collapsed");
+    document.getElementById("carat").style.transform = "rotate(180deg)";
+  } else {
+    filters.style.height = "98px";
+    filters.classList.add("collapsed");
+    document.getElementById("carat").style.transform = "rotate(0deg)";
+  }
+}
+
 export default Parks;
--- src/components/Recreation.js
+++ src/components/Recreation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import NotFound from './NotFound';
 import StateCard from './StateCard';
@@ -139,10 +139,6 @@ class Recreation extends React.Component {
       <Route component={NotFound} />
     );
   }
-}
-
-function getSlug(pre, str) {
-  return "/" + pre + "/" + str.replace(/\s+/g, '-').toLowerCase();
 }
 
 export default GoogleApiWrapper({
