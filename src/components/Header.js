66d4f13e8a0fc0cacab0b66043be9a33dbea97b4
jengelhart <alexanderengelhart713@gmail.com>
2019-11-08 12:05:33 -0600

    global search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,85 @@
 import React from 'react';
+import Fuse from 'fuse.js';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import Logo from '../assets/img/logo.png';
 
+const LOC_ENDPOINT = "https://flask-backend-dot-potent-retina-254722.appspot.com/api/locations";
+const REC_ENDPOINT = "https://flask-backend-dot-potent-retina-254722.appspot.com/api/recreations";
+const PARK_ENDPOINT = "https://flask-backend-dot-potent-retina-254722.appspot.com/api/nationalparks";
+
 class Header extends React.Component {
+
+  search() {
+    let searchString = document.getElementById("searchField").value
+    fetch(LOC_ENDPOINT) //+ "?q="+ JSON.stringify(this.state.query))
+
+      // Transform the data into json
+      .then((resp) => resp.json())
+      // Search
+      .then((data) => {
+        const options = {
+          keys: ['name'],
+        };
+        const fuse = new Fuse(data['objects'], options)
+        console.log(fuse.search(searchString))
+      })
+
+      // .then((data) => {
+      //   this.setState({numPages: data.total_pages});
+      //   // Process data
+      //   data.objects.forEach((park) => {
+      //     this.state.states.push(park);
+      //   });
+      // }).then(() => {
+      //   this.setState({loaded: true});
+      // });
+    fetch(REC_ENDPOINT) //+ "?q="+ JSON.stringify(this.state.query))
+
+      // Transform the data into json
+      .then((resp) => resp.json())
+      // Search
+      .then((data) => {
+        const options = {
+          keys: ['rec_name'],
+        };
+        const fuse = new Fuse(data['objects'], options)
+        console.log(fuse.search(searchString))
+      })
+
+      // .then((data) => {
+      //   this.setState({numPages: data.total_pages});
+      //   // Process data
+      //   data.objects.forEach((park) => {
+      //     this.state.states.push(park);
+      //   });
+      // }).then(() => {
+      //   this.setState({loaded: true});
+      // });
+    fetch(PARK_ENDPOINT) //+ "?q="+ JSON.stringify(this.state.query))
+
+      // Transform the data into json
+      .then((resp) => resp.json())
+      // Search
+      .then((data) => {
+        const options = {
+          keys: ['park_name'],
+        };
+        const fuse = new Fuse(data['objects'], options)
+        console.log(fuse.search(searchString))
+      })
+
+      // .then((data) => {
+      //   this.setState({numPages: data.total_pages});
+      //   // Process data
+      //   data.objects.forEach((park) => {
+      //     this.state.states.push(park);
+      //   });
+      // }).then(() => {
+      //   this.setState({loaded: true});
+      // });
+  }
 
   render() {
     return (
@@ -30,8 +107,8 @@ class Header extends React.Component {
             </ul>
 
             <form className="form-inline my-2 my-lg-0">
-              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="button button-primary my-2 my-sm-0" type="submit">Search</button>
+              <input className="form-control mr-sm-2" id="searchField" type="search" placeholder="Search" aria-label="Search"/>
+              <FontAwesomeIcon icon={faSearch} className="model-search-component" onClick={this.search}/>
             </form>
           </div>
         </div>
