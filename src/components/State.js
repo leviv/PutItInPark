115b18ebb510aa1c97d541173e97d32fba13c828
Levi Villarreal <leviv@wireless-10-145-139-78.public.utexas.edu>
2019-11-08 16:54:00 -0600

    Add recreations to state page

--- src/components/State.js
+++ src/components/State.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import ParkCard from './ParkCard';
+import RecreationCard from './RecreationCard';
 import NotFound from './NotFound';
 
 const API_ENDPOINT = "https://flask-backend-dot-potent-retina-254722.appspot.com/api";
@@ -13,6 +14,7 @@ class State extends React.Component {
 
     this.state = {
       state: [],
+      recs: [],
       parks: [],
       stateName: stateName,
       loaded: false
@@ -26,30 +28,46 @@ class State extends React.Component {
       .then((data) => {
         // Process data
         data.park_names = data.park_names.split(",");
+        data.rec_ids = data.rec_ids.split(",");
         this.setState({state: data});
       }).then(() => {
-        let numParksLoaded = 0;
+        let numRecsLoaded = 0;
+        this.state.state.rec_ids.forEach((id) => {
+          const query = {"filters":[{"name":"rec_id","op":"eq","val":id}], "single":true};
+          fetch(API_ENDPOINT + "/recreations?q=" + JSON.stringify(query))
+            // Transform the data into json
+            .then((resp) => resp.json())
+            .then((data) => {
+              // Process data
+              this.state.recs.push(data);
+            }).then(() => {
+              numRecsLoaded++;
+              if (numRecsLoaded === this.state.state.rec_ids.length) {
+                let numParksLoaded = 0;
 
-        // Check if we need to load any parks
-        if (this.state.state.park_names.length === 0 || this.state.state.park_names[0] === "N/A") {
-          this.setState({loaded: true});
-        } else {
-          // Get the park information
-          this.state.state.park_names.forEach((id) => {
-            fetch(API_ENDPOINT + "/nationalparks/" + id)
-              // Transform the data into json
-              .then((resp) => resp.json())
-              .then((data) => {
-                // Process data
-                this.state.parks.push(data);
-              }).then(() => {
-                numParksLoaded++;
-                if (numParksLoaded === this.state.state.park_names.length) {
+                // Check if we need to load any parks
+                if (this.state.state.park_names.length === 0 || this.state.state.park_names[0] === "N/A") {
                   this.setState({loaded: true});
+                } else {
+                  // Get the park information
+                  this.state.state.park_names.forEach((id) => {
+                    fetch(API_ENDPOINT + "/nationalparks/" + id)
+                      // Transform the data into json
+                      .then((resp) => resp.json())
+                      .then((data) => {
+                        // Process data
+                        this.state.parks.push(data);
+                      }).then(() => {
+                        numParksLoaded++;
+                        if (numParksLoaded === this.state.state.park_names.length) {
+                          this.setState({loaded: true});
+                        }
+                      });
+                  });
                 }
-              });
-          });
-        }
+              }
+            });
+        });
       });
   }
 
@@ -60,8 +78,12 @@ class State extends React.Component {
   render() {
     // Valid park
     if (this.state.loaded){
-      const row = this.state.parks.map((x,i) => {
+      const parksRow = this.state.parks.map((x,i) => {
         return i % 4 === 0 ? this.state.parks.slice(i, i+4) : null;
+      }).filter(x => x != null);
+
+      const recRow = this.state.recs.map((x,i) => {
+        return i % 4 === 0 ? this.state.recs.slice(i, i+4) : null;
       }).filter(x => x != null);
 
       return (
@@ -89,10 +111,10 @@ class State extends React.Component {
 
             <h3>National Parks</h3>
             {this.state.state.park_names[0] === "N/A" &&
-              <h4>No Parks</h4>
+              <h4>No Parks<br/><br/></h4>
             }
 
-            {row.map((result, index) => {
+            {parksRow.map((result, index) => {
               return (
                 <div className="row" key={index}>
                   {result.map((item, innerIndex) => {
@@ -103,6 +125,7 @@ class State extends React.Component {
                           imglink={item.imglink}
                           location={item.location}
                           num_rec = {item.num_rec}
+                          visitors = {item.visitors}
                           fee={item.fee}
                         />
                       </div>
@@ -111,6 +134,27 @@ class State extends React.Component {
                 </div>
               );
            })}
+
+           <h3>Recreational Areas</h3>
+           {recRow.map((result, index) => {
+             return (
+               <div className="row" key={index}>
+                 {result.map((item, innerIndex) => {
+                   return (
+                     <div className="col-md-3 instance-container" key={innerIndex}>
+                       <RecreationCard
+                         rec_name={item.rec_name}
+                         imglink={item.imglink}
+                         num_activities={item.num_activities}
+                         reservable={item.reservable}
+                         stay_limit={item.stay_limit}
+                       />
+                     </div>
+                   );
+                 })}
+               </div>
+             );
+          })}
           </div>
         </React.Fragment>
       );
