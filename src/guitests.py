8a85435e999f69f71e9b92ba287f08b3e751eef9
jengelhart <alexanderengelhart713@gmail.com>
2019-11-08 19:29:06 -0600

    accept CI 7

--- src/guitests.py
+++ src/guitests.py
@@ -16,7 +16,7 @@ class guitests(TestCase):
     @classmethod
     def setUp(self):
         webdriver.ChromeOptions().add_argument("--no-sandbox")
-        self.browser = webdriver.Chrome("/usr/bin/google-chrome")
+        self.browser = webdriver.Chrome("/usr/bin/chromedriver")
         self.url = url
 
     @classmethod
