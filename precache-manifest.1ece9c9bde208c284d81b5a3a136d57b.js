6e603626491c940ca5832a63d9e87d2eb78242db
Levi Villarreal <villarreallevi@gmail.com>
2020-11-24 04:14:34 -0600

    Change structure to support deployment via heroku

---
+++ precache-manifest.1ece9c9bde208c284d81b5a3a136d57b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d1713d72301393ea6e0c4800e94138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef09106e29df590b53a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9d4a24aa7fbb45aa8e43",
+    "url": "/static/css/main.5d5629ff.chunk.css"
+  },
+  {
+    "revision": "7ef09106e29df590b53a",
+    "url": "/static/js/2.aa172188.chunk.js"
+  },
+  {
+    "revision": "9d4a24aa7fbb45aa8e43",
+    "url": "/static/js/main.3aad3e37.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9bf74faa8abb3c41451ce72f7ac81b2",
+    "url": "/static/media/Billy.b9bf74fa.png"
+  },
+  {
+    "revision": "855b27c29cb9ff94ab363ffc0e7f6742",
+    "url": "/static/media/Ella.855b27c2.png"
+  },
+  {
+    "revision": "89f469748842f9d90a2e6e747792dcd7",
+    "url": "/static/media/Joseph.89f46974.png"
+  },
+  {
+    "revision": "156b4ad8dd8cd056f887cc2dddf66ef2",
+    "url": "/static/media/Levi.156b4ad8.png"
+  },
+  {
+    "revision": "0d0d060e5b1e7a76826ec9c0a253271d",
+    "url": "/static/media/Miles.0d0d060e.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "9ce24d9b7339fdf39ca961e5870cca34",
+    "url": "/static/media/chrome.9ce24d9b.jpeg"
+  },
+  {
+    "revision": "e37848ea37184d297ce3f9e298867020",
+    "url": "/static/media/climbing.e37848ea.jpg"
+  },
+  {
+    "revision": "ef9bf7a6b628dab5cadd1fb7dbddbb6e",
+    "url": "/static/media/cloudsqlproxy.ef9bf7a6.jpeg"
+  },
+  {
+    "revision": "cefb6899d698beaddb53b44c466cba7a",
+    "url": "/static/media/d3.cefb6899.png"
+  },
+  {
+    "revision": "7517ad6c962636e14e932eaba21b3bd5",
+    "url": "/static/media/docker.7517ad6c.jpeg"
+  },
+  {
+    "revision": "ed1889be2c05028ff74d6608f80ca2a3",
+    "url": "/static/media/express.ed1889be.jpeg"
+  },
+  {
+    "revision": "e232224ca8f47430ecfd0ef01e8f09da",
+    "url": "/static/media/flask.e232224c.jpeg"
+  },
+  {
+    "revision": "0564b847e09cd00c61e741b29b997f07",
+    "url": "/static/media/gcp.0564b847.png"
+  },
+  {
+    "revision": "4e481e0d2e2fd142bf9ecb88c4613e75",
+    "url": "/static/media/gitlab.4e481e0d.png"
+  },
+  {
+    "revision": "80c8348f6c6340aefedb8f9b00dc8771",
+    "url": "/static/media/logo.80c8348f.png"
+  },
+  {
+    "revision": "a4bb638fed74148a073d0240a0f9a323",
+    "url": "/static/media/mocha.a4bb638f.png"
+  },
+  {
+    "revision": "255b77e251b19a6d0600634d2ff9b006",
+    "url": "/static/media/mysql.255b77e2.png"
+  },
+  {
+    "revision": "9300e7998e6fc2b04e81eea94b7ce544",
+    "url": "/static/media/namecheap.9300e799.png"
+  },
+  {
+    "revision": "9ae8ceaa543d93d321a63d4f6f1b120d",
+    "url": "/static/media/postman.9ae8ceaa.png"
+  },
+  {
+    "revision": "f34770503b90f26ea389f557500ff825",
+    "url": "/static/media/react.f3477050.png"
+  },
+  {
+    "revision": "5c20cdc1ed8fb0462e0ba69b03a96705",
+    "url": "/static/media/scss.5c20cdc1.png"
+  },
+  {
+    "revision": "e3a8cc80c6b4feb9e935a84ac1f18613",
+    "url": "/static/media/selenium.e3a8cc80.png"
+  },
+  {
+    "revision": "f20986510f9fc841963a82db4565a2e5",
+    "url": "/static/media/splash.f2098651.png"
+  },
+  {
+    "revision": "91e39757e5d22f2f970f5f64e4b5136f",
+    "url": "/static/media/texas.91e39757.jpg"
+  },
+  {
+    "revision": "88733d26bdbbac1b798bbcd57c3caa1c",
+    "url": "/static/media/us-state-names.88733d26.tsv"
+  },
+  {
+    "revision": "07ef38d950cedd4f1f729b531068062f",
+    "url": "/static/media/yosemite.07ef38d9.jpg"
+  }
+]);
